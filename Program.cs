6f470cd5f27c5344bd6ddab23f7ed8640eea9b86
Your Name <nagimov2016@inbox.ru>
2023-03-01 12:22:05 +0300

    Homework#1

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// #Вторая задача
+
+// Console.Write("Введите первое число: ");
+// int firstValue = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondValue = Convert.ToInt32(Console.ReadLine());
+// int max = firstValue;
+// if (secondValue > max)
+// {
+//     Console.WriteLine(secondValue + " Максимальное число");
+// }
+// else 
+// {
+//     Console.WriteLine(firstValue + " Максимальное число");
+// }
+
+// #Четвертая задача
+
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+
+// if (b > max ) max = b;
+// if (c > max ) max = c;
+
+// Console.WriteLine( max + "=>" + "максимальное число" );
+
+// #Шестая задача
+
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if ( N % 2 == 1 )
+// {
+// Console.WriteLine( N + " ->" + " нечетное" );
+// }
+// else
+// {
+// Console.WriteLine( N + " ->" + " четное" );
+// } 
+
+// #Восьмая задача
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int a = 1;
+// while(a <= n)
+//     if (a % 2 == 1)
+//         a++;
+//     else
+//         {
+//         Console.WriteLine(a);
+//         a++;
+//         } 
